631a6c68986eba61434a16a302e470592abb6a15
Piotrek <pioterek01@gmail.com>
2014-06-14 20:32:55 +0200

    Hello world new view - index and routing in urls.py

--- SESJA/urls.py
+++ SESJA/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from Sesja import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SESJA.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
