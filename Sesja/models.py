3a9c1b5dca3a2cbeb7aa02b58a58c47c3cd54b65
Anna Konopka <aniakonopka@example.com>
2014-06-14 20:46:02 +0200

    Added model Modelik

--- Sesja/models.py
+++ Sesja/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Modelik(models.Model):
+    slowa = models.CharField(max_length=150)
+    data = models.DateTimeField('data_nasza')
+    liczba = models.IntegerField(default=21)
+
